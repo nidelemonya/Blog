0c47870715ac07f21d85e5ef0eb5fb6cc0ec6ce8
周子寒 <wb-zzh846742@alibaba-inc.com>
2021-01-22 15:09:16 +0800

    blog 更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,12 +24,12 @@ module.exports = {
      * 'tomorrow'
      */
     author: '你的lemon呀',
-    authorAvatar: '/logo.jpg',
+    authorAvatar: '/logo.jpeg',
     // 备案
     record: '赣ICP备20009270号',
     recordLink: 'https://beian.miit.gov.cn/',
     // 项目开始时间，只填写年份
-    startYear: '2020',
+    startYear: '2021',
     noFoundPageByTencent: false,
     codeTheme: 'okaidia', // default 'tomorrow'
     sidebarDepth: 2, // e'b将同时提取markdown中 h2 和 h3 标题，显示在侧边栏上。
@@ -37,7 +37,7 @@ module.exports = {
     nav: [ //    导航栏配置
       {
         text: '笔记',
-        link: '/pages/note/js/inherit.html',
+        link: '/pages/note/JavaScript/JavaScript知识点总结.html',
         icon: 'reco-document'
       }, // 内部链接 以docs为根目录
       {
@@ -72,15 +72,20 @@ module.exports = {
           sidebarDepth: 2,
           children: [
             ['../pages/note/JavaScript/async-await.md','async-await'],
-            ['../pages/note/JavaScript/防抖和节流.md','防抖和节流'],
             ['../pages/note/JavaScript/deep-clone.md','深克隆'],
-            ['../pages/note/JavaScript/继承与原型链.md','继承与原型链'], // 以docs为根目录来查找文件 
+            ['../pages/note/JavaScript/Event-loop.md','事件循环'],
             ['../pages/note/JavaScript/instanceof.md','实现instanceof'],
+            ['../pages/note/JavaScript/JavaScript知识点总结.md','JavaScript知识点总结'],
             ['../pages/note/JavaScript/new.md','请你实现一个new'],
             ['../pages/note/JavaScript/object.create.md','实现object.create'],
             ['../pages/note/JavaScript/Promise.md','Promise'],
+            ['../pages/note/JavaScript/Proxy.md','Proxy'],
             ['../pages/note/JavaScript/this.md','this'],
+            ['../pages/note/JavaScript/webpack.md','webpack'],
+            ['../pages/note/JavaScript/从输入url到页面加载完成发生了啥.md','从输入url到页面加载完成发生了啥'],
+            ['../pages/note/JavaScript/继承与原型链.md','继承与原型链'], // 以docs为根目录来查找文件 
             ['../pages/note/JavaScript/箭头函数和普通函数的区别.md','箭头函数和普通函数的区别'],
+            ['../pages/note/JavaScript/防抖和节流.md','防抖和节流'],
           ]
         },
         {
@@ -89,7 +94,9 @@ module.exports = {
           sidebarDepth: 2,
           children: [
             ['../pages/note/计算机网络/Http协议.md','Http协议'],
-            ['../pages/note/计算机网络/Http缓存.md','Http缓存']
+            ['../pages/note/计算机网络/Http缓存.md','Http缓存'],
+            ['../pages/note/计算机网络/网络安全.md','网络安全'],
+            ['../pages/note/计算机网络/跨域.md','跨域'],
           ]
         },
         {
@@ -97,7 +104,15 @@ module.exports = {
           collapsable:true, 
           sidebarDepth: 2,
           children: [
-            ['../pages/note/Css/Css.md','Css']
+            ['../pages/note/Css/Css.md','Css'],
+          ]
+        },
+        {
+          title: '面经', 
+          collapsable:true, 
+          sidebarDepth: 2,
+          children: [
+            ['../pages/note/面经/子寒-面经.md','子寒-面经'],
           ]
         }
       ]
